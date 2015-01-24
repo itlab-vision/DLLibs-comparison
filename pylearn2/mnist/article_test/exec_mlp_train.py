95047d68d9638abe5afa27c84dc034c5661d848f
evgeniy <evgeniy@evgeniy-Aspire-V3-771.(none)>
2015-01-24 16:49:41 +0300

    test net

--- pylearn2/mnist/article_test/exec_mlp_train.py
+++ pylearn2/mnist/article_test/exec_mlp_train.py
@@ -2,15 +2,11 @@ import os
 import pylearn2
 from pylearn2.config import yaml_parse
 
-path = os.path.join(pylearn2.__path__[0], 'scripts', 'tutorials', 'multilayer_perceptron', 'mlp_tutorial_part_4.yaml')
+path = os.path.join('mlp.yaml')
 with open(path, 'r') as f:
      train_3 = f.read()
 hyper_params = {'train_stop' : 50000,
                 'valid_stop' : 60000,
-                'dim_h0' : 500,
-                'dim_h1' : 1000,
-                'sparse_init_h1' : 15,
-                'max_epochs' : 10000,
                 'save_path' : '.'}
 train_3 = train_3 % (hyper_params)
 
